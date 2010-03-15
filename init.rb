867f97c470241ff7f72035b421f9e6cdb2640a6f
Phil Gengler <phil@pgengler.net>
2010-03-15 19:51:39 +0000

    Update version number to 0.0.2 to represent Image: support
    git-svn-id: file:///srv/svn/redmine-mediawiki-formatter/trunk@4 7d7f7033-cb0b-46ed-8934-ef90726bbff3

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_mediawiki_formatter do
 	name 'Mediawiki formatter'
 	author 'Phil Gengler'
 	description 'This provides Mediawiki syntax (like Wikipedia) as a wiki format'
-	version '0.0.1'
+	version '0.0.2'
  
 	wiki_format_provider 'mediawiki', RedmineMediawikiFormatter::WikiFormatter, RedmineMediawikiFormatter::Helper
 end
